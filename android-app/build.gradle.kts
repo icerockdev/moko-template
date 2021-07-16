810f7f9ca7b398684f8e1a11d4802881aa68e929
denischornyy <dchornyy@icerockdev.com>
2021-07-16 10:44:58 +0700

    fix hilt

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -24,6 +24,15 @@ android {
     }
 }
 
+kapt {
+    javacOptions {
+        // These options are normally set automatically via the Hilt Gradle plugin, but we
+        // set them manually to workaround a bug in the Kotlin 1.5.20
+        option("-Adagger.fastInit=ENABLED")
+        option("-Adagger.hilt.android.internal.disableAndroidSuperclassValidation=true")
+    }
+}
+
 dependencies {
     implementation(libs.appCompat)
     implementation(libs.material)
