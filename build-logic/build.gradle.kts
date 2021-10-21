2b8e9d7587eef5dc7283edccc8a2a2b6dd6d18a1
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2021-10-21 10:10:09 +0700

    kotlin 1.6.0 rc test

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 dependencies {
     api("dev.icerock:mobile-multiplatform:0.12.0")
-    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0-RC")
     api("com.android.tools.build:gradle:7.0.0")
-    api("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.18.0")
+    api("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.18.1")
 }
